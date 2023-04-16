dbe1d5319c343b4d6411c98ca9f0075e0233a087
Robin van Ee <robin@rvanee.nl>
2023-04-16 14:33:12 +0200

    Implement consumable data component

--- Code/Components/ConsumableData.cs
+++ Code/Components/ConsumableData.cs
@@ -1,9 +1,11 @@
 ﻿using WolfRPG.Core;
+using WolfRPG.Core.Statistics;
 
 namespace WolfRPG.Inventory
 {
 	public class ConsumableData: IRPGComponent
 	{
-		
+		public AttributeStatusEffect[] AttributeStatusEffects { get; set; }
+		public SkillStatusEffect[] SkillStatusEffects { get; set; }
 	}
 }
