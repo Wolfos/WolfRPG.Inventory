6559765d1a6f9d6a97e8960a415a6cd63e891766
Robin <robin@rvanee.nl>
2023-06-25 14:53:28 +0200

    Shops

---
+++ Code/Components/ShopComponent.cs
@@ -0,0 +1,10 @@
+﻿using WolfRPG.Core;
+
+namespace WolfRPG.Inventory
+{
+	public class ShopComponent: IRPGComponent
+	{
+		public string[] StartingInventory { get; set; }
+		public int BarteringMoney { get; set; }
+	}
+}
