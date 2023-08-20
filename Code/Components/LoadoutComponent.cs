d63717583183b96a591ad9a32872236af49b1976
Robin <robin@rvanee.nl>
2023-08-20 18:21:43 +0200

    Use the new nicer GUI for LoadoutComponent

--- Code/Components/LoadoutComponent.cs
+++ Code/Components/LoadoutComponent.cs
@@ -3,8 +3,15 @@ using WolfRPG.Core;
 
 namespace WolfRPG.Inventory
 {
+	public class ItemReference
+	{
+		[DBReference(1)] public RPGObjectReference ItemObject { get; set; }
+		public int Quantity { get; set; } = 1;
+		public bool Equipped { get; set; }
+	}
+	
 	public class LoadoutComponent: IRPGComponent
 	{
-		public string[] StartingInventory { get; set; }
+		public ItemReference[] StartingInventory { get; set; }
 	}
 }
