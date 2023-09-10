4443d990e709fd0b168ecc4f85463209c6510b96
Robin <robin@rvanee.nl>
2023-09-10 14:11:03 +0200

    Animation sets for equipment

--- Code/Components/EquipmentData.cs
+++ Code/Components/EquipmentData.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using WolfRPG.Character;
 using WolfRPG.Core;
 
@@ -13,11 +14,18 @@ namespace WolfRPG.Inventory
 	{
 		public EquipmentSlot EquipmentSlot { get; set; }
 		
-		// All visual equipment pieces are defined in the character prefab, this defines which pieces get activated by this equipment item
+		// All visual equipment pieces (except weapons) are defined in the character prefab, this defines which pieces get activated by this equipment item
 		// Most clothing items are composed of multiple parts. For example, a pair of pants would affect hips, left leg, and right leg
 		public EquipmentPart[] EquipmentParts { get; set; }
 		
 		[DBReference(4)] // category 4 = status effects
 		public RPGObjectReference StatusEffect { get; set; }
+
+		// Use the prefab set in ItemData
+		public bool UsePrefab { get; set; }
+		
+		public bool OverrideAnimations { get; set; }
+		[AssetReference(typeof(AnimatorOverrideController))]
+		public AssetReference AnimationSet { get; set; }
 	}
 }
