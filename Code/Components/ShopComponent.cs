6dde730252f0ea8e2e4510884ae01e9c34ed5968
Robin <robin@rvanee.nl>
2023-10-08 21:00:05 +0200

    ShopComponent update

--- Code/Components/ShopComponent.cs
+++ Code/Components/ShopComponent.cs
@@ -2,9 +2,17 @@
 
 namespace WolfRPG.Inventory
 {
+	public class ShopItem
+	{
+		[DBReference(1)] // category 1 = items
+		public RPGObjectReference Item { get; set; }
+
+		public int Quantity { get; set; } = 1;
+	}
+	
 	public class ShopComponent: IRPGComponent
 	{
-		public string[] StartingInventory { get; set; }
+		public ShopItem[] ShopInventory { get; set; }
 		public int BarteringMoney { get; set; }
 	}
 }
